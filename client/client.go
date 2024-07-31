ffb4aa01d0a7810eb38d261a1718da7f6585a8dd
Luiz Alexandre Silva <luiz.sarmento@fpf.br>
2024-07-31 16:30:20 -0400

    challenge solved

---
+++ client/client.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
+	defer cancel()
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	f, err := os.Create("cotacao.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = f.WriteString("Dólar: " + string(body))
+}
